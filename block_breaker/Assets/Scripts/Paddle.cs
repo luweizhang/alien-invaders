9bc5381c505e17bb580e47d1d5c82f6a3bf61e36
Willie Wang <luwzhang@gmail.com>
2020-06-25 00:05:08 -0700

    stick to paddle

--- block_breaker/Assets/Scripts/Paddle.cs
+++ block_breaker/Assets/Scripts/Paddle.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
 
     // Update is called once per frame
     void Update() {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
