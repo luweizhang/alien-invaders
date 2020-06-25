e405132b12489f309d0c16082e16c3e0c5875979
Willie Wang <luwzhang@gmail.com>
2020-06-25 15:58:04 -0700

    add collision detection

--- block_breaker/Assets/Scripts/Ball.cs
+++ block_breaker/Assets/Scripts/Ball.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class Ball : MonoBehaviour { 
-
+public class Ball : MonoBehaviour
+{
     //config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 35f;
 
     //state
-    Vector2 paddleToBallVector; 
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +23,25 @@ public class Ball : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
 
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
     }
 }
