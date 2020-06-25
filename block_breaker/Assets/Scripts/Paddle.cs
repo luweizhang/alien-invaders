bc05679c628ade4afffca54634b9eeef68b19ed6
Willie Wang <luwzhang@gmail.com>
2020-06-24 19:49:48 -0700

    update mouse position

---
+++ block_breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Update is called once per frame
+    void Update() {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
